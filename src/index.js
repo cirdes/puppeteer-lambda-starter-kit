30466bde9b523ff50e26491df0ff08b060f28646
Cirdes Henrique <cirdes@gmail.com>
2018-07-27 15:20:12 -0300

    recfatoring

--- src/index.js
+++ src/index.js
@@ -20,38 +20,35 @@ exports.run = async (browser, params) => {
     cnpj = params;
   }
 
-  console.log('Node VERSION');
-  console.log(process.version);
-
+  // page creating and setup
   const page = await browser.newPage();
   await page.setViewport({width: 1024, height: 720});
   await page.setDefaultNavigationTimeout(60000);
 
+  // get ready to dismiss dialogs
   page.on('dialog', async (dialog) => {
     await dialog.dismiss();
   });
 
+  // goes to page
   await page.goto('http://efisco.sefaz.pe.gov.br/sfi_trb_gpf/PREmitirCertidaoNegativaNarrativaDebitoFiscal',
    {waitUntil: ['domcontentloaded', 'networkidle0']}
   );
-  console.log('cdTipoDocumento');
-  // await page.waitFor('#cdTipoDocumento');
-  await page.select('#cdTipoDocumento', '2');
-  console.log('cdOrgaoEmissor');
-  await page.select('select#cdOrgaoEmissor', '1');
-  console.log('nuDocumento');
-  console.log(cnpj);
-  await page.type('#nuDocumento', cnpj);
-  console.log('about to click');
 
+  // input data to forms
+  await page.select('#cdTipoDocumento', '2');
+  await page.select('select#cdOrgaoEmissor', '1');
+  await page.type('#nuDocumento', cnpj);
+
+  // Go to download page
   await page.evaluate(() => {
     const element = document.querySelector('#btt_emitir');
     element.click();
   });
   await page.waitForNavigation();
 
+  // Set download path and click
   await page.waitFor('input[name="btt_pmu_acao_4"]');
-  console.log('ja chegou');
   await page._client.send('Page.setDownloadBehavior',
     {behavior: 'allow', downloadPath: '/tmp/'});
   await page.evaluate(() => {
@@ -59,32 +56,25 @@ exports.run = async (browser, params) => {
     element.click();
   });
   await page.waitFor(5000);
-  console.log('clicked');
 
-  // Init download
-
-  // await page.screenshot({path: '/tmp/screenshot.png'});
-  const s3 = new aws.S3({apiVersion: '2006-03-01'});
+  // read pdf from disk
   const pdf = await new Promise((resolve, reject) => {
     fs.readFile('/tmp/RelatorioCertidaoNegativaInscrito.pdf', (err, data) => {
       if (err) return reject(err);
       resolve(data);
     });
   });
-  console.log('downloaded');
+
+  // Uplado to S3 Bucket
+  const s3 = new aws.S3({apiVersion: '2006-03-01'});
   await s3.putObject({
     ACL: 'public-read',
     Bucket: 'screenshots-debug',
     Key: 'RelatorioCertidaoNegativaInscrito.pdf',
     Body: pdf,
   }).promise();
-  console.log('uploaded');
-  // await page.waitFor('#btt_emitir');
-  // await Promise.all([
-  //   page.waitForNavigation(),
-  //   page.click('#btt_emitir'),
-  // ]);
 
+  // response in API Gateway Format
   const response = {
        statusCode: 200,
        body: JSON.stringify({pdf: 'https://s3.amazonaws.com/screenshots-debug/RelatorioCertidaoNegativaInscrito.pdf'}),
